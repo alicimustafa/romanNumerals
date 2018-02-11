495ac92aa959fe68fcc6f3667633798c4ce2fb3f
mustafa alici <mustafaalici@mustafas-MacBook-Pro.local>
2018-02-11 16:21:16 -0700

    first commit

---
+++ src/main/java/com/mustafa/roman/RomanNumeralTranslatorApplication.java
@@ -0,0 +1,12 @@
+package com.mustafa.roman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RomanNumeralTranslatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RomanNumeralTranslatorApplication.class, args);
+	}
+}
